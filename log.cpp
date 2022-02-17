d0829377f1c114c45c92e02a1c78d047d3744f6d
Rofirger <842474373@qq.com>
2022-02-17 11:14:47 +0800

    2022-2-17 first commit

--- log.cpp
+++ log.cpp
@@ -7,6 +7,8 @@
 #include <io.h>
 #include <filesystem>
 #include <stdio.h>
+#include <chrono>
+#include <vector>
 
 #if defined(_MSC_VER)
 #include <direct.h>
@@ -44,6 +46,11 @@ rofirger::Log::~Log()
 	}
 }
 
+void rofirger::Log::SetMsgBufferSize(size_t _s_)noexcept
+{
+	_msg_buffer_size = _s_;
+}
+
 void rofirger::Log::SetFolderPath(const char* _folder_path_)noexcept
 {
 	_folder_path = _folder_path_;
@@ -60,10 +67,10 @@ bool rofirger::Log::StartLog()
 		if (NULL == GetCurrentDir(current_folder, 256)) return false;
 		sprintf(new_log_folder, "%s\\log\\", current_folder);
 		_folder_path = new_log_folder;
-		if (stat(new_log_folder, &info) == 0) goto point_log_startlog_func_break_if;
+		if (stat(new_log_folder, &info) == 0) goto POS_LOG_STARTLOG_FUNC_BREAK_IF;
 		if (-1 == makedir(new_log_folder)) return false;
 	}
-point_log_startlog_func_break_if:
+POS_LOG_STARTLOG_FUNC_BREAK_IF:
 	time_t now_time = time(NULL);
 	struct tm* t = localtime(&now_time);
 	char log_file_path[256];
@@ -83,34 +90,39 @@ void rofirger::Log::StopLog()
 	_thread->join();
 }
 
-void rofirger::Log::AddLog(LOG_LEVEL _log_level_, const char* _file_, size_t _line_num_, const char* _func_sig_, const char* psz_fmt_, ...)
+void rofirger::Log::AddLog(LOG_LEVEL _log_level_, const char* _file_, size_t _line_num_, const char* _func_sig_, const char* fmt_, ...)
 {
-	char msg[10240] = { 0 };
-	va_list vArgList;
-	va_start(vArgList, psz_fmt_);
-	vsnprintf(msg, 10240, psz_fmt_, vArgList);
-	va_end(vArgList);
+	std::vector<char> msg;
+	msg.resize(_msg_buffer_size);
+	va_list ap;
+	va_start(ap, fmt_);
+	vsnprintf(msg.data(), msg.size(), fmt_, ap);
+	va_end(ap);
 
 	time_t now = time(NULL);
 	struct tm* tmstr = localtime(&now);
-	char content[10500] = { 0 };
-	sprintf(content, "[%04d-%02d-%02d %02d:%02d:%02d][%s][0x%04x][FILE:%s LINE:%d FUNC:%s]  %s\n",
+	char complete_log[10500] = { 0 };
+	auto chrono_time_now = std::chrono::system_clock::now();
+	auto chrono_msecs = std::chrono::duration_cast<std::chrono::milliseconds>(chrono_time_now.time_since_epoch()).count()
+		- std::chrono::duration_cast<std::chrono::seconds>(chrono_time_now.time_since_epoch()).count() * 1000;
+	sprintf(complete_log, "[%04d-%02d-%02d %02d:%02d:%02d.%03d][%s][0x%04x][FILE:%s LINE:%d FUNC:%s][MSG:%s]\n",
 		tmstr->tm_year + 1900,
 		tmstr->tm_mon + 1,
 		tmstr->tm_mday,
 		tmstr->tm_hour,
 		tmstr->tm_min,
 		tmstr->tm_sec,
+		chrono_msecs,
 		_level_str[(int)_log_level_].c_str(),
 		std::this_thread::get_id(),
 		_file_,
 		_line_num_,
 		_func_sig_,
-		msg);
+		msg.data());
 
 	{
 		std::lock_guard<std::mutex> guard(_mutex);
-		_queue_log_data.push(content);
+		_queue_log_data.push(complete_log);
 	}
 	_cv.notify_one();
 }
@@ -129,15 +141,34 @@ void rofirger::Log::ThreadFunc()
 			if (_is_stop) return;
 			_cv.wait(lock_);
 		}
+		CheckFileSize();
 		const std::string& str_ = _queue_log_data.front();
 		fwrite((void*)str_.c_str(), str_.length(), 1, _ptr_file);
 		fflush(_ptr_file);
 		_queue_log_data.pop();
 	}
 }
-void main()
+
+void rofirger::Log::CheckFileSize()
 {
-	rofirger::Log::GetInstance()->StartLog();
-	rofirger::log(rofirger::LOG_LEVEL_INFO, "%s,%d", "hello", 5);
-	rofirger::Log::GetInstance()->StopLog();
+	struct stat info;
+	constexpr long log_file_overflow_size = 1024 * 1024;
+	if (stat(_file_path.c_str(), &info) == 0)
+	{
+		_off_t file_size_ = info.st_size; // the number of bytes of the file
+		if (file_size_ > log_file_overflow_size)
+		{
+		POS_LOG_RESTART_MAKE_NEW_FILE:
+			time_t now_time = time(NULL);
+			struct tm* t = localtime(&now_time);
+			char log_file_path[256];
+			sprintf(log_file_path, "%s%04d%02d%02d%02d%02d%02d.log", _folder_path.c_str(), t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+			_file_path = log_file_path;
+			do { _ptr_file = fopen(log_file_path, "wt+"); } while (_ptr_file == NULL);
+		}
+	}
+	else
+	{
+		goto POS_LOG_RESTART_MAKE_NEW_FILE;
+	}
 }
--- log.h
+++ log.h
@@ -15,25 +15,28 @@ namespace rofirger
 	{
 		LOG_LEVEL_INFO = 0x00,
 		LOG_LEVEL_WARNING = 0x01,
-		LOG_LEVEL_ERROR = 0x02
+		LOG_LEVEL_ERROR = 0x02,
+		LOG_LEVEL_FATAL = 0x03
 	}LOG_LEVEL;
-#define log(_log_level_, ...) Log::GetInstance()->AddLog(_log_level_,__FILE__,__LINE__,__FUNCSIG__,__VA_ARGS__)
+#define add_log(_log_level_, ...) Log::GetInstance()->AddLog(_log_level_,__FILE__,__LINE__,__FUNCSIG__,__VA_ARGS__)
 	class Log
 	{
 	public:
 		static Log* GetInstance();
+		void SetMsgBufferSize(size_t _s_)noexcept;
 		void SetFolderPath(const char* _folder_path_)noexcept;
 		bool StartLog();
 		void StopLog();
-		void AddLog(LOG_LEVEL _log_level_, const char* _file_, size_t _line_num_, const char* _func_sig_, const char* psz_fmt_, ...);
+		void AddLog(LOG_LEVEL _log_level_, const char* _file_, size_t _line_num_, const char* _func_sig_, const char* fmt_, ...);
 	private:
 		Log() = default;
 		~Log();
 		Log(const Log& _log_) = delete;
 		Log& operator=(const Log& _log_) = delete;
 		void ThreadFunc();
+		void CheckFileSize();
 	private:
-		std::string _level_str[3]{ "INFO","WARNING","ERROR" };
+		std::string _level_str[4]{ "INFO","WARNING","ERROR","FATAL" };
 		static Log* _ptr_log;
 		std::mutex _mutex;
 		static std::mutex _mutex_get_instance;
@@ -44,6 +47,7 @@ namespace rofirger
 		std::queue<std::string> _queue_log_data;
 		std::shared_ptr<std::thread>    _thread;
 		bool _is_stop;
+		size_t _msg_buffer_size = 1024; // default:1024, the size can be reassigned.
 	};
 }
 #endif
